5eadafc605d96cf6fb2c07997ad099d8a8144848
davdik <70813712+davdik@users.noreply.github.com>
2020-11-07 12:35:18 +0200

    Update score.py

--- score.py
+++ score.py
@@ -21,6 +21,7 @@ def get_hit_count():
 def score():
     file = open("C:\Temp\MemoryScore.txt", "r")
     print(file.read())
+    return file
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
